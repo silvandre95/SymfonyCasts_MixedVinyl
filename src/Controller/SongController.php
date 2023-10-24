7bd38992fedbe20c10053b718bc4fa605e613e85
Andre Silva <geral@digi2code.pt>
2023-10-24 23:05:38 +0100

    done

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -10,19 +10,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
-    public function getSong(int $id, LoggerInterface $logger) : Response
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
-       $song = [
-           'id' => $id,
-           'name' => 'Waterfalls',
-           'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3'
-       ];
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
 
-       $logger->info('Returning API responde for a song {song}', [
-           'song' => $id
-       ]);
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
 
-       return new JsonResponse($song);
+        return $this->json($song);
     }
 }
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
-    #[Route(path: '/', name: 'app_homepage')]
-    public function homepage(Environment $twig): Response
+    #[Route('/', name: 'app_homepage')]
+    public function homepage(): Response
     {
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
@@ -22,15 +21,13 @@ class VinylController extends AbstractController
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
 
-        $html = $twig->render('vinyl/homepage.html.twig', [
+        return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
         ]);
-
-        return new Response($html);
     }
 
-    #[Route(path: '/browse/{slug}', name: 'app_browse')]
+    #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
